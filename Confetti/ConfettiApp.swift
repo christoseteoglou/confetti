54b72c641d166aef01ba06983f0a5e8308b8461f
christoseteoglou <itsmeglue@gmail.com>
2023-11-26 20:00:02 +0100

    Initial Commit

---
+++ Confetti/ConfettiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettiApp.swift
+//  Confetti
+//
+//  Created by Christos Eteoglou on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct ConfettiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
